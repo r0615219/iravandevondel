dbcaabb241ff4fc67940548371d67bec51fd34d1
Caroline Boeykens <carolineboeykens@hotmail.com>
2019-01-06 22:33:12 +0100

    initial commit

---
+++ modules/custom/src/services/CustomService.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Custom module for Craft CMS 3.x
+ *
+ * Some cool functions here
+ *
+ * @link      http://carolineboeykens.be
+ * @copyright Copyright (c) 2018 Caroline Boeykens
+ */
+
+namespace modules\custom\services;
+
+use GuzzleHttp\Client;
+use modules\custom\Custom;
+
+use Craft;
+use craft\base\Component;
+
+/**
+ * CustomService Service
+ *
+ * All of your module’s business logic should go in services, including saving data,
+ * retrieving data, etc. They provide APIs that your controllers, template variables,
+ * and other modules can interact with.
+ *
+ * https://craftcms.com/docs/plugins/services
+ *
+ * @author    Caroline Boeykens
+ * @package   Custom
+ * @since     1.0.0
+ */
+class CustomService extends Component
+{
+
+}
