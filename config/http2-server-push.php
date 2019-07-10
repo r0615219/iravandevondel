ccb498e6f5f42ed7927f69b18183143113e03180
Caroline Boeykens <carolineboeykens@hotmail.com>
2019-07-10 20:56:21 +0200

    update

---
+++ config/http2-server-push.php
@@ -0,0 +1,10 @@
+<?php
+
+// Configuring HTTP2 Server Push
+return [
+    // Limit how many tags to include in the Link tag
+    'limit' => null,
+
+    // Include images
+    'includeImages' => false,
+];
